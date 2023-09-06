40a03249c44efd6419088e6b1de4e6b0d42a841d
AndreaB <128647787+andreaber23@users.noreply.github.com>
2023-09-06 15:19:54 -0400

    fixed manifest in server.js

--- server/server.js
+++ server/server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
-const routes = require('./routes');
 const { ApolloServer } = require('apollo-server-express') //apollo server
 const { typeDefs, resolvers } = require('./schemas');
 
@@ -14,16 +13,10 @@ const server = new ApolloServer({
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(routes)
 
-// if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
-
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/public/index.html'));
-});
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
+};
 
 const startApolloServer = async () => {
   await server.start();
@@ -35,7 +28,7 @@ const startApolloServer = async () => {
       console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
     })
   })
-  };
+};
 
-  startApolloServer();
+startApolloServer();
  
